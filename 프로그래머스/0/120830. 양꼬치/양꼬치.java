ddcb6b33c8e368686e4708e19d3d09836755015b
yanadooseul <dltmfrl0608@gmail.com>
2025-09-23 21:08:38 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        int free = 0;
+        
+            free = n/10;
+            answer += (n*12000) + (k*2000) - (free*2000);
+        return answer;
+    }
+}
