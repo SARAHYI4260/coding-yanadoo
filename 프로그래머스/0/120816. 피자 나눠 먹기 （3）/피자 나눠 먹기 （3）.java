1a03e3ddce27d2c0d6b012a756da3cb9b071cc9a
SARAHYI <dltmfrl0608@gmail.com>
2026-04-15 13:27:09 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int slice, int n) {
+        int answer = n;
+        int i = 1;
+        
+        for(i = 1; (i*slice)<n; i++){
+          
+        }answer = i;
+        return answer;
+    }
+}
