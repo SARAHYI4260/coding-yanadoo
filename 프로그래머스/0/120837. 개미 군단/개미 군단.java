0a0fd32309a7620daadf032a467528621bc5b79e
SARAHYI <dltmfrl0608@gmail.com>
2026-04-30 20:36:06 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -1,18 +1,13 @@
 class Solution {
     public int solution(int hp) {
         int answer = 0;
-        int major = 5, soldier = 3, worker = 1;
-        int num1 = 0, num2 = 0, num3 =0;
-        int rest1 = 0, rest2 = 0;
-        
-        num1 = hp / major;
-        rest1 = hp % major;
-        num2 = rest1 / soldier;
-        rest2 = rest1 % soldier;
-        num3 = rest2 / worker;
-        
-        answer = num1 + num2 + num3;
-        
+        int[] ants = {5, 3, 1};
+
+        for (int ant : ants) {
+            answer += hp / ant;
+            hp %= ant;
+        }
+
         return answer;
     }
 }
